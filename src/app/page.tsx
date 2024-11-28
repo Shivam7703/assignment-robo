f6778cce9bdfc2db3592377403134ee191451b13
shivam7703 <shivamg7703@gmail.com>
2024-11-28 20:38:48 +0530

    l

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HomeBanner from "@/components/banner-slides/HomeBanner";
 import MostLoved from "@/components/banner-slides/MostLoved";
 import VibeSlide from "@/components/banner-slides/VibeSlide";
+import { logo } from "@/assets";
+import Image from "next/image";
 
 export default function Home() {
   return (
    <>
-   <div className=" h-36  w-full"></div>
+   <div className=" h-36 py-2 w-full">
+    <Image src={logo} alt="logo" className="h-full w-full"/>
+   </div>
   <HomeBanner/>
   <VibeSlide/>
   <MostLoved/>
